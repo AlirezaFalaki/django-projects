2e0e9b09b6359eeda6322ee9a6dfd2407b01ea37
AlirezaFalaki <alireza.falaki.8047@gmail.com>
2022-09-05 11:39:45 +0430

    adding login and register template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "dashboard"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='main'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 from django.views.generic import TemplateView
 
@@ -7,3 +7,8 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'main.html')
+
+
+class RegisterView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register.html')
