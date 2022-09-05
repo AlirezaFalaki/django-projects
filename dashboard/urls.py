b31d3ca52e431a866a82bf01f38a786142538460
AlirezaFalaki <alireza.falaki.8047@gmail.com>
2022-09-05 15:14:42 +0430

    complete register and logout

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = "dashboard"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='main'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
 ]
+
+# path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name="login"),
+# path('logout/', auth_views.LogoutView.as_view(template_name='user/landing.html'), name="logout"),
+# path('register/', register, name="register"),
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.contrib.auth.models import User
 
 
 class IndexView(TemplateView):
@@ -9,6 +11,39 @@ class IndexView(TemplateView):
         return render(request, 'main.html')
 
 
+class LoginView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html')
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print('salam')
+            return redirect('/')
+        return HttpResponse('Your entered information is not correct !!!')
+
+
 class RegisterView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'register.html')
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('Cpassword')
+
+        if password == cpassword:
+            User.objects.create(username=username, email=email, password=password)
+            return redirect('dashboard:main')
+        return HttpResponse('invalid input data !!!.')
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('/')
